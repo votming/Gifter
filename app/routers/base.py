5f6bf1ec2b6e4f5e0e94dfb12f001a7206641617
Ilya Mashintsev <ilya.mashintsev@gcore.com>
2023-05-10 16:47:10 +0400

    Change filtration for generator

--- app/routers/base.py
+++ app/routers/base.py
@@ -23,50 +23,53 @@ def get_title(
         offer_hero_first_name: str,
         offer_hero_second_name: str,
         db: Session = Depends(get_db),
-        language: str = None,
-        gender: str = None,
-        country: str = None,
-        tv_show: str = None,
-        titles: str = None,
-        featuring_image: str = None,
-        paragraphs: str = None
+        language: str | None = None,
+        gender: str | None = None,
+        country: str | None = None,
+        tv_show: str | None = None,
+        titles: str | None = None,
+        featuring_image: str | None = None,
+        paragraphs: str | None = None
 ):
     try:
         response = {}
         offer_name = f'{offer_hero_first_name} {offer_hero_second_name}'
-        title_query = db.query(Title)
-        if titles:
-            title_query = title_query.filter(Title.id.in_(json.loads(titles)))
-        if gender:
-            title_query = title_query.filter(Title.gender == gender)
-        if country:
-            title_query = title_query.join(Country, isouter=True).filter(or_(Country.code == country, Title.country_id == None))
-        if language:
-            title_query = title_query.join(Language, isouter=True).filter(or_(Language.code == language, Title.language_id == None))
-        title = title_query.order_by(func.random()).first()
-        if title is None:
-            return 'No title records were found with desired settings'
-        if title.country is None or title.country.currency is None:
-            currency = db.query(Currency).get(1)
-        else:
-            currency = title.country.currency
-        demonym = title.country.demonym if title.country is not None else 'people'
-        text = title.template.format(offer_name=offer_name, offer_hero=offer_name, country_demonym_capitalized=demonym, currency_symbol=currency.symbol)#{**title.__dict__, **title.country.__dict__, **title.language.__dict__})
-        response['title'] = text
 
-        paragraph_query = db.query(Paragraph)
-        if paragraphs:
-            paragraph_query = paragraph_query.filter(Paragraph.id.in_(json.loads(paragraphs)))
-        if country:
-            paragraph_query = paragraph_query.join(Country, isouter=True).filter(or_(Country.code == country, Paragraph.country_id == None))
-        if language:
-            paragraph_query = paragraph_query.join(Language, isouter=True).filter(or_(Language.code == language, Paragraph.language_id==None))
-        paragraph = paragraph_query.order_by(func.random()).first()
-        if paragraph is not None:
-            print(paragraph.country)
-            bank_name = paragraph.country.bank_name if paragraph.country is not None else 'Bank'
-            text = paragraph.template.format(offer_name=offer_name,offer_hero=offer_name, tv_show_name=tv_show, currency_symbol=currency.symbol, CENTRAL_BANK=bank_name, central_bank=bank_name, offer_hero_first_name=offer_hero_first_name,offer_hero_second_name=offer_hero_second_name)
-            response['paragraph'] = text
+        if titles is not None:
+            title_query = db.query(Title)
+            if titles != 'any':
+                title_query = title_query.filter(Title.id.in_(json.loads(titles)))
+            if gender:
+                title_query = title_query.filter(or_(Title.gender == gender, Title.gender == None))
+            if country:
+                title_query = title_query.join(Country, isouter=True).filter(or_(Country.code == country, Title.country_id == None))
+            if language:
+                title_query = title_query.join(Language, isouter=True).filter(or_(Language.code == language, Title.language_id == None))
+            title = title_query.order_by(func.random()).first()
+            if title is None:
+                return 'No title records were found with desired settings'
+            if title.country is None or title.country.currency is None:
+                currency = db.query(Currency).get(1)
+            else:
+                currency = title.country.currency
+            demonym = title.country.demonym if title.country is not None else 'people'
+            text = title.template.format(offer_name=offer_name, offer_hero=offer_name, country_demonym_capitalized=demonym, currency_symbol=currency.symbol)#{**title.__dict__, **title.country.__dict__, **title.language.__dict__})
+            response['title'] = text
+
+        if paragraphs is not None:
+            paragraph_query = db.query(Paragraph)
+            if paragraphs!= 'any':
+                paragraph_query = paragraph_query.filter(Paragraph.id.in_(json.loads(paragraphs)))
+            if country:
+                paragraph_query = paragraph_query.join(Country, isouter=True).filter(or_(Country.code == country, Paragraph.country_id == None))
+            if language:
+                paragraph_query = paragraph_query.join(Language, isouter=True).filter(or_(Language.code == language, Paragraph.language_id == None))
+            paragraph = paragraph_query.order_by(func.random()).first()
+            if paragraph is not None:
+                print(paragraph.country)
+                bank_name = paragraph.country.bank_name if paragraph.country is not None else 'Bank'
+                text = paragraph.template.format(offer_name=offer_name,offer_hero=offer_name, tv_show_name=tv_show, currency_symbol=currency.symbol, CENTRAL_BANK=bank_name, central_bank=bank_name, offer_hero_first_name=offer_hero_first_name,offer_hero_second_name=offer_hero_second_name)
+                response['paragraph'] = text
 
         return response
     except Exception as ex:
